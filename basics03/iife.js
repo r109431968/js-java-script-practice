64e03abd5e84d51722f4e96b7a7a16f9e8476fb2
Rahul Tiwari <40773470+r109431968@users.noreply.github.com>
2024-11-20 12:03:55 +0000

    Invoked Function

--- basics03/iife.js
+++ basics03/iife.js
@@ -2,20 +2,20 @@
 
 (function chai() {
     console.log(`DB Connected 1st !!!`);
-}) (); // ye semicolon lga diya.
+}) (); // Ye invoked function hai or ye semicolon is liye lgaya taki dusra invoked function 
+       // run ho usme error na aye, is liye semicolon lgana jruri hai.
 
 (function aurCode() {
     console.log(`DB Connected 2nd !!! `);
-    
 }) ();
 
 ( () => {
     console.log(`DB Connected 3rd !!! `);
-    
 }) ();
 
 // Both code are same, pr run krne me error hai, reason ?
-// Invoked Function hai dono but jb hm code run kr rhe hain to invoked function kahan stop hona hai ye nhi btaya us k liye hme invoked function k last me semicolon(;) lgana padega.
+// Invoked Function hai dono but jb hm code run kr rhe hain to invoked function kahan stop 
+// hona hai ye nhi btaya, us k liye hme invoked function k last me semicolon(;) lgana padega.
 // example mene chai code k immediate invoked function k last me semicolon(;) lga diya.
 
 ( (name) => {
