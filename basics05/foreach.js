336e73be41c025a87b44341daf5637db38c6f57b
Rahul Tiwari <40773470+r109431968@users.noreply.github.com>
2024-11-21 21:19:01 +0000

    foreach

---
+++ basics05/foreach.js
@@ -0,0 +1,45 @@
+const code = ["js", "py", "rb", "c#", "jq"]
+
+// These are all the way t write or print a object as function using foreach.
+// code.forEach(function (val) {
+//     console.log(val);
+// });
+
+
+// code.forEach( (item) => {
+//     console.log(item);
+    
+// });
+
+
+// function printMe(item) {
+//     console.log(item);
+// }
+// code.forEach(printMe)
+
+
+// code.forEach((item, index, arr) => {
+//     console.log(item, index, arr); // it will print an array.
+// })
+
+
+const coding = [
+    {
+        languagename: "java",
+        languagefilename: "java"
+    },
+    {
+        languagename: "python",
+        languagefilename: "py"
+    },
+    {
+        languagename: "C#",
+        languagefilename: "c#"
+    }
+]
+
+coding.forEach((item) => 
+{
+    console.log(item.languagename);
+    
+})
