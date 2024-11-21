ef652ae40d5814642c69127f6572222e5f220442
Rahul Tiwari <40773470+r109431968@users.noreply.github.com>
2024-11-21 20:50:20 +0000

    forof

---
+++ basics05/forof.js
@@ -0,0 +1,69 @@
+const arr = [1, 2, 3, 4, 5]
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// for (const num of arr) {
+//     if(num == 3){
+//         console.log(`Break when ${num} is detected, Okayy !!`);
+//         break
+//     }
+//     console.log(num);
+// }
+
+
+//  const greetings = "Greetings"
+//  for(const greet of greetings){
+//     console.log(`Each characte is ${greet}`);
+//  }
+
+
+//  const greetings = "Hello World !!!"
+//  for(const greet of greetings){
+//     console.log(`Each characte is ${greet}`);
+//  }
+
+
+//Map
+// const map = new Map()
+// map.set('IN', 'INDIA')
+// map.set('USA', 'United States of America')
+// map.set('Fr', 'FRANCE')
+// map.set('IN', 'INDIA') // it will not add this it takes unique values.
+
+// console.log(map);
+// // console.log(typeof(map));
+
+// for (const key of map) {
+//     console.log(key);    // It change into array.   
+// }
+
+
+// for (const [key, value] of map) {
+//     console.log(key, '-', value);    // It change into iterate.   
+// }
+
+// Object can not iterate but above we can see MAP can be iterate or MAP bhi key value pair or bject bhi key value pair hai.
+// const myObject = {
+//     'game1': 'NFS',
+//     'geme2': 'Spiderman'
+// }
+
+// for (const [key, value] of myObject) {
+//     console.log(key, '-', value);
+// }
+
+
+const myObject = {
+    game1: 'NFS',
+    geme2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, '-', value);
+// }
+
+// Ye iterate nhi ho rha ye forin se iterate hoga object k liye forin use hota hai.
+// Is ko smjhne k liye forin.js file open kro jo ki basics05 me hai. 
+
